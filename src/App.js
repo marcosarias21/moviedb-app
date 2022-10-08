cc95fb7a9689a6692366c0a5db2eecb9582e0ea7
Marcos Arias <johndoe@example.com>
2022-10-08 16:55:13 -0300

    add custom hook called usefetch and url with api key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import useFetch from './hooks/useFetch';
+
+const API_KEY = process.env.REACT_APP_MOVIEDB_API_KEY;
 
 const App = () => {
-  const API_KEY = process.env.REACT_APP_MOVIEDB_API_KEY;
+  const { data } = useFetch(`https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`);
+  console.log(data);
   return (
    <>
    </>
