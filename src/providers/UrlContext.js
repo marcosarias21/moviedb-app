4311663f28e2e5393ac0e2676b0eb740fa60b418
marcosarias21 <marcosariasolaiz@gmail.com>
2022-10-19 11:17:17 -0300

    added tv show detail and a helper for set section

--- src/providers/UrlContext.js
+++ src/providers/UrlContext.js
@@ -3,9 +3,12 @@ import { createContext, useState } from 'react';
 const urlContext = createContext();
 
 export const UrlProvider = ({ children }) => {
-  const [movieCategory, setMovieCategory] = useState('popular');
+  const [movieCategory, setMovieCategory] = useState('movie/popular');
+  const [section, setSection] = useState('movie');
 
-  const data = { movieCategory, setMovieCategory };
+  const data = {
+    movieCategory, setMovieCategory, section, setSection,
+  };
 
   return (<urlContext.Provider value={data}>{children}</urlContext.Provider>);
 };
