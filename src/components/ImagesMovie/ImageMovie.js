b0f97565d539c52fa844ed5320724104ce80dcfd
Marcos Arias <johndoe@example.com>
2022-10-28 16:51:27 -0300

    responsive in laptop

--- src/components/ImagesMovie/ImageMovie.js
+++ src/components/ImagesMovie/ImageMovie.js
@@ -6,7 +6,7 @@ const ImageMovie = ({ backdrops }) => {
   return (
       <div className="row gx-0">
         <h2 className='text-center'> Images </h2>
-        {data?.map((backdrop, i) => (
+        {data?.map((backdrop) => (
         <div key={backdrop.file_path} className="mb-4 col-sm-12 col-md-6 col-lg-4">
           <img className='img-media-q' src={`https://image.tmdb.org/t/p/w400//${backdrop.file_path}`}/>
         </div>))}
